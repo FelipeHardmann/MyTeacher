421751a947740399c0c4027ea86c1a36b229b208
FelipeHardmann <fashardmann@gmail.com>
2023-07-25 16:54:53 -0300

    :sparkles: feat a props

--- frontend/src/components/Lista/Lista.tsx
+++ frontend/src/components/Lista/Lista.tsx
@@ -1,21 +1,34 @@
 import { Button } from "@mui/material"
-import { ListaStyled, ItemLista, Foto, Informacoes, Nome, Valor, Descricao } from "./Lista.style"
+import { ListaStyled, ItemLista, Foto, Informacoes, Nome, Valor, Descricao, ListaVazia } from "./Lista.style"
+import { Professor } from "@/src/@types/professor"
 
-const Lista = () => {
+interface ListaProfessores {
+    professores: Professor[],
+}
+
+const Lista = (props: ListaProfessores) => {
     return(
-        <ListaStyled>
-            
-            <ItemLista>
-                <Foto src="https://github.com/FelipeHardmann.png"></Foto>
-                <Informacoes>
-                    <Nome>Felipe Hardmann</Nome>
-                    <Valor>R$ 100,00</Valor>
-                    <Descricao>Aulas de programação</Descricao>
-                    <Button>Marcar Aula</Button>
-                </Informacoes>
-            </ItemLista>
-            
-        </ListaStyled>
+        <div>
+            {props.professores.length > 0 ? (
+                <ListaStyled>
+                    {props.professores.map(professor => (
+                    <ItemLista key={professor.id}>
+                        <Foto src={professor.foto}></Foto>
+                        <Informacoes>
+                            <Nome>{professor.nome}</Nome>
+                            <Valor>R$ {professor.valor_hora} por hora</Valor>
+                            <br />
+                            <Descricao>{professor.descricao}</Descricao>
+                            <br />
+                            <Button sx={{width: '70%'}}>Marcar Aula com Felipe</Button>
+                        </Informacoes>
+                    </ItemLista>
+                    ))}
+                </ListaStyled>
+            ) : (
+                <ListaVazia>Nenhum item encontrado</ListaVazia>
+            )}
+            </div>    
     )
 }
 
