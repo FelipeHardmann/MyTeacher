e8a813f072a03ef385da46a414b67cdb71d02e03
FelipeHardmann <fashardmann@gmail.com>
2023-07-27 17:14:09 -0300

    :wrench: refactore descricao

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -7,28 +7,28 @@ export default function Home() {
     {
       id: 1,
       nome: 'Professor 1',
-      descricao: 'Descração do professor 1',
+      descricao: 'Descrição do professor 1',
       valor_hora: 100,
       foto: 'https://github.com/FelipeHardmann.png'
     },
     {
       id: 2,
       nome: 'Professor 2',
-      descricao: 'Descração do professor 2',
+      descricao: 'Descrição do professor 2',
       valor_hora: 100,
       foto: 'https://github.com/FelipeHardmann.png'
     },
     {
       id: 3,
       nome: 'Professor 3',
-      descricao: 'Descração do professor 3',
+      descricao: 'Descrição do professor 3',
       valor_hora: 100,
       foto: 'https://github.com/FelipeHardmann.png'
     },
     {
       id: 4,
       nome: 'Professor 4',
-      descricao: 'Descração do professor 4',
+      descricao: 'Descrição do professor 4',
       valor_hora: 100,
       foto: 'https://github.com/FelipeHardmann.png'
     }
