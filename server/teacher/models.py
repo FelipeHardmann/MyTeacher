2f2149cd787b87124260e14bc6e80875b804ad37
FelipeHardmann <fashardmann@gmail.com>
2023-08-02 15:58:09 -0300

    :wrench: chore files

--- server/teacher/models.py
+++ server/teacher/models.py
@@ -6,3 +6,16 @@ class Professor(models.Model):
     valor_hora = models.DecimalField(max_digits=9, decimal_places=2, null=False, blank=False)
     descricao = models.TextField(null=False, blank=False)
     foto = models.URLField(max_length=255, null=False, blank=False)
+
+
+class Aula(models.Model):
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    email = models.EmailField(max_length=255, blank=False, null=False)
+    professor = models.ForeignKey(
+        to=Professor, 
+        on_delete=models.CASCADE, 
+        related_name='aulas', 
+        null=False, 
+        blank=False
+    )
+    
