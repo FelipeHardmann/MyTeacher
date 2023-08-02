425543cb24346e57e6ccc91b779dc038e9ddc08d
FelipeHardmann <fashardmann@gmail.com>
2023-08-02 15:56:46 -0300

    :sparkles: feat new API

--- server/teacher/views.py
+++ server/teacher/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from teacher.models import Professor
+from teacher.serializers import ProfessorSerializer
+from rest_framework import status
 
-# Create your views here.
+
+class ProfessorView(APIView):
+    def get(self, request, format=None):
+        professores = Professor.objects.all()
+        serializer = ProfessorSerializer(professores, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class AulaView(APIView):
+    def post(self, request, id, format=None):
+        ...
