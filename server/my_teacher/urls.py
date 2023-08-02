3bab2a731131a208c1d26dbe810b97194a562483
FelipeHardmann <fashardmann@gmail.com>
2023-08-02 15:55:38 -0300

    :sparkles: feat new urls

--- server/my_teacher/urls.py
+++ server/my_teacher/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-
+from teacher.views import ProfessorView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('professores/', include('teacher.urls'))
 ]
