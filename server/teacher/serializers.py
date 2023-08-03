23af12fa7dc80271ed5d1c8e569362187fcb0968
FelipeHardmann <fashardmann@gmail.com>
2023-08-03 02:26:34 -0300

    :sparkles: feat validate name

--- server/teacher/serializers.py
+++ server/teacher/serializers.py
@@ -1,3 +1,4 @@
+from django.forms import ValidationError
 from rest_framework import serializers
 from teacher.models import Aula, Professor
 
@@ -11,6 +12,11 @@ class ProfessorSerializer(serializers.ModelSerializer):
 class CadastrarAulaSerializer(serializers.Serializer):
     email = serializers.EmailField(max_length=255)
     nome = serializers.CharField(max_length=100)
+
+    def validate_nome(self, value):
+        if len(value) < 3:
+            raise ValidationError('Deve ter pelo menos 4 caracteres')
+        return value
 
 
 class AulaSerializer(serializers.ModelSerializer):
