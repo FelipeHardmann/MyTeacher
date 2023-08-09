c0075afac5b586d3a9f91a73b076980ebffbe45b
FelipeHardmann <fashardmann@gmail.com>
2023-08-09 17:57:57 -0300

    :sparkles: feat: new Dialog

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,12 +1,42 @@
 import { Professor } from "@/src/@types/professor";
 import Lista from "@/src/components/Lista/Lista";
 import { useIndex } from "@/src/hooks/pages/useIndex";
-import { Box } from "@mui/material";
+import { Box, Button, Dialog, DialogActions, Grid, TextField } from "@mui/material";
 
 export default function Home() {
   const { listaProfessores } = useIndex()
   
   return (
+    <div>
       <Lista professores={listaProfessores}></Lista>
+    
+      <Dialog open={true} fullWidth PaperProps={{sx: {p: 5}}}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <TextField 
+              label='Digite o Nome'
+              type='text'
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField 
+              label='Digite o Email'
+              type='email'
+              fullWidth
+            />
+          </Grid>
+        </Grid>
+
+        <DialogActions sx={{mt: 5}}>
+          <Button>
+            Cancelar
+          </Button>
+          <Button>
+            Marcar
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
   )
 }
