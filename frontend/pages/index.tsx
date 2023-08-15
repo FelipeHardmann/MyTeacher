cbe7f37a8b00c811918cc1aaf61f0c2182b908e3
FelipeHardmann <fashardmann@gmail.com>
2023-08-15 16:43:11 -0300

    :sparkles: feat: new snackbar

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,22 +1,38 @@
 import { Professor } from "@/src/@types/professor";
 import Lista from "@/src/components/Lista/Lista";
 import { useIndex } from "@/src/hooks/pages/useIndex";
-import { Box, Button, Dialog, DialogActions, Grid, TextField } from "@mui/material";
+import { Box, Button, Dialog, DialogActions, Grid, Snackbar, TextField, Alert } from "@mui/material";
 
 export default function Home() {
-  const { listaProfessores } = useIndex()
+  const { 
+    listaProfessores, 
+    nome, 
+    setNome, 
+    email,
+    setEmail,
+    professorSelecionado,
+    setProfessorSelecionado,
+    marcarAula,
+    mensagem,
+    setMensagem 
+  } = useIndex()
   
   return (
     <div>
-      <Lista professores={listaProfessores}></Lista>
+      <Lista 
+        professores={listaProfessores}
+        onSelect={(professor) => setProfessorSelecionado(professor)}
+      ></Lista>
     
-      <Dialog open={true} fullWidth PaperProps={{sx: {p: 5}}}>
+      <Dialog onClose={() => setProfessorSelecionado(null)} open={professorSelecionado != null} fullWidth PaperProps={{sx: {p: 5}}}>
         <Grid container spacing={2}>
           <Grid item xs={12}>
             <TextField 
               label='Digite o Nome'
               type='text'
               fullWidth
+              value={nome}
+              onChange={(e) => setNome(e.target.value)}
             />
           </Grid>
           <Grid item xs={12}>
@@ -24,19 +40,67 @@ export default function Home() {
               label='Digite o Email'
               type='email'
               fullWidth
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </Grid>
         </Grid>
 
         <DialogActions sx={{mt: 5}}>
-          <Button>
+          <Button onClick={() => setProfessorSelecionado(null)}>
             Cancelar
           </Button>
-          <Button>
+          <Button onClick={() => marcarAula()}>
             Marcar
           </Button>
         </DialogActions>
       </Dialog>
+
+      <Snackbar 
+        open={mensagem === 'Cadastrado com sucesso'} 
+        autoHideDuration={2500} 
+        onClose={() => setMensagem('')}
+      >
+          <Alert 
+            severity="success" 
+            onClose={() => setMensagem('')}
+            >
+              {mensagem}
+          </Alert>  
+      </Snackbar>
+    
+      <Snackbar
+        open={mensagem === 'Houveram erros de validação'}
+        autoHideDuration={2500}
+        onClose={() => setMensagem('')}
+      >
+          <Alert
+            severity="error"
+            onClose={() => setMensagem('')}
+          >
+            {mensagem}
+          </Alert>
+      </Snackbar>
+    
+      <Snackbar
+        open={mensagem === 'Preencha os dados corretamente'}
+        autoHideDuration={2500}
+        onClose={() => setMensagem('')}
+      >
+          <Alert
+            severity="warning"
+            onClose={() => setMensagem('')}
+          >
+              {mensagem}
+          </Alert>
+      </Snackbar>
+
     </div>
   )
 }
+
+      //   message={mensagem} 
+      //   open={mensagem.length > 0} 
+      //   autoHideDuration={2500}
+      //   onClose={() => setMensagem('')}
+      // />
